b5f57cd1622432be61d21f364cd7c888fcf49a92
unknown <pakphil7@gmail.com>
2020-07-20 13:33:29 -0700

    changed the mongodb_uri

--- server.js
+++ server.js
@@ -9,12 +9,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client"));
 }
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booklist");
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://heroku_qm7c1t8d:1234qwer@ds239936.mlab.com:39936/heroku_qm7c1t8c"
+);
 
 app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
